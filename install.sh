6ad3875b26cb7b9d6e3f883b2177ccf560ce1c2f
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-05-21 18:39:24 +0300

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Default variables
 function="install"
-NODE="0g"
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
@@ -25,12 +24,6 @@ if [ -f "$bash_profile" ]; then
 	    . $HOME/.bash_profile
 fi
 
-export DAEMON_HOME=$HOME/.0gchain
-export DAEMON_NAME=0gchaind
-if [ -d "$DAEMON_HOME" ]; then
-    new_folder_name="${DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
-    mv "$DAEMON_HOME" "$new_folder_name"
-fi
 if [ ! $OG_ALIAS ]; then
 	read -p "Enter validator name: " OG_ALIAS
 	echo 'export OG_ALIAS='\"${OG_ALIAS}\" >> $HOME/.bash_profile
@@ -52,131 +45,75 @@ echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile &
 go version
 
 cd $HOME
-rm -rf 0g-evmos
-rm -rf 0g-chain
-git clone -b v0.1.0 https://github.com/0glabs/0g-chain.git
-./0g-chain/networks/testnet/install.sh
-source .profile
-SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656"
-$DAEMON_NAME init "${OG_ALIAS}" --chain-id zgtendermint_9000-1
-sleep 1
-$DAEMON_NAME config keyring-backend test
-$DAEMON_NAME config chain-id zgtendermint_16600-1
-wget -O $DAEMON_HOME/config/genesis.json https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json
-sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $DAEMON_HOME/config/config.toml
+############
+# Clone project repository
+cd && rm -rf 0g-chain
+git clone https://github.com/0glabs/0g-chain
+cd 0g-chain
+git checkout v0.1.0
 
-echo "[Unit]
-Description=$NODE Node
-After=network.target
+# Build binary
+make install
 
+# Set node CLI configuration
+0gchaind config chain-id zgtendermint_16600-1
+0gchaind config keyring-backend test
+0gchaind config node tcp://localhost:26657
+
+# Initialize the node
+0gchaind init "$OG_ALIAS" --chain-id zgtendermint_16600-1
+
+# Download genesis and addrbook files
+curl -L https://snapshots-testnet.nodejumper.io/0g-testnet/genesis.json > $HOME/.0gchain/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/0g-testnet/addrbook.json > $HOME/.0gchain/config/addrbook.json
+
+# Set seeds
+sed -i -e 's|^seeds *=.*|seeds = "c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656"|' $HOME/.0gchain/config/config.toml
+
+# Set minimum gas price
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0025ua0gi"|' $HOME/.0gchain/config/app.toml
+
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  $HOME/.0gchain/config/app.toml
+
+# Download latest chain data snapshot
+curl "https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.0gchain"
+#change ports
+sed -i.bak -e "s%:26658%:33658%; s%:26657%:33657%; s%:6060%:6760%; s%:26656%:33656%; s%:26660%:33660%" $HOME/.0gchain/config/config.toml && sed -i.bak -e "s%:9090%:9790%; s%:9091%:9791%; s%:1317%:2017%; s%:8545%:9245%; s%:8546%:9246%; s%:6065%:6765%" $HOME/.0gchain/config/app.toml && sed -i.bak -e "s%:26657%:33657%" $HOME/.0gchain/config/client.toml 
+# Create a service
+sudo tee /etc/systemd/system/0gchaind.service > /dev/null << EOF
+[Unit]
+Description=0G node service
+After=network-online.target
 [Service]
 User=$USER
-Type=simple
 ExecStart=$(which 0gchaind) start
 Restart=on-failure
+RestartSec=10
 LimitNOFILE=65535
-
 [Install]
-WantedBy=multi-user.target" > $HOME/$NODE.service
-sudo mv $HOME/$NODE.service /etc/systemd/system
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
+WantedBy=multi-user.target
 EOF
-
-wget -O $DAEMON_HOME/config/addrbook.json https://snapshots.nodes.guru/og/addrbook.json
-
-echo -e '\n\e[42mChecking a ports\e[0m\n' && sleep 1
-#CHECK PORTS
-PORT=335
-if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
-    echo -e "\e[31mPort 26656 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
-    echo -e "\e[31mPort 26657 already in use\e[39m"
-    sleep 2
-    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
-    sleep 2
-    $DAEMON_NAME config node tcp://localhost:${PORT}57
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
-    echo -e "\e[31mPort 26658 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
-    echo -e "\e[31mPort 6060 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
-    echo -e "\e[31mPort 1317 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
-    echo -e "\e[31mPort 9090 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":9091$" ; then
-    echo -e "\e[31mPort 9091 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:9091\"|:${PORT}91\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 9091 changed to ${PORT}91.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":8545$" ; then
-    echo -e "\e[31mPort 8545 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:8545\"|:${PORT}45\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 8545 changed to ${PORT}45.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":8546$" ; then
-    echo -e "\e[31mPort 8546 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:8546\"|:${PORT}46\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 8546 changed to ${PORT}46.\e[0m\n"
-    sleep 2
-fi
-echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
-sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
-sudo systemctl enable $NODE
-sudo systemctl restart $NODE
+sudo systemctl enable 0gchaind.service
 
-echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ `service $NODE status | grep active` =~ "running" ]]; then
-  echo -e "Your $NODE node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice 0g status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your $NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
+# Start the service and check the logs
+sudo systemctl start 0gchaind.service
+sudo journalctl -u 0gchaind.service -f --no-hostname -o cat
+############
+
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-    sudo systemctl disable ogd.service
-    sudo rm /etc/systemd/system/ogd.service
-    sudo rm -rf $HOME/0g-evmos $HOME/.evmosd
-        sudo systemctl stop 0g
-        sudo systemctl disable 0g
-        sudo rm -rf $(which 0gchaind) $HOME/.0gchain
+    sudo systemctl disable 0gchaind.service
+    sudo rm /etc/systemd/system/0gchaind.service
+    sudo rm -rf $HOME/.0gchain $HOME/0gchain
     echo "Done"
     cd $HOME
     ;;
