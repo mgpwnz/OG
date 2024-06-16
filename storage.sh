5da9979ef1de01fe595777e1e6dc6fc358e479f4
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-06-16 14:05:22 +0300

    update

--- storage.sh
+++ storage.sh
@@ -38,77 +38,64 @@ echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile &
 go version
 
 cd $HOME
-############
 # Clone project repository
-git clone https://github.com/0glabs/0g-storage-node.git
+git clone -b v0.2.0 https://github.com/0glabs/0g-storage-node.git
 cd $HOME/0g-storage-node
-git fetch
-git checkout tags/v0.2.0
-git submodule update — init
+git submodule update --init
 sudo apt install cargo
-cargo build — release
+cargo build --release
+# var
+echo 'export ZGS_LOG_DIR="$HOME/0g-storage-node/run/log"' >> ~/.bash_profile
+echo 'export ZGS_LOG_CONFIG_FILE="$HOME/0g-storage-node/run/log_config"' >> ~/.bash_profile
+echo 'export LOG_CONTRACT_ADDRESS="0x2b8bC93071A6f8740867A7544Ad6653AdEB7D919"' >> ~/.bash_profile
+echo 'export MINE_CONTRACT="0x228aCfB30B839b269557214216eA4162db24445d"' >> ~/.bash_profile
 
+source ~/.bash_profile
 
+read -sp "Enter your private key: " PRIVATE_KEY && echo
 
-# Set node CLI configuration
-0gchaind config chain-id zgtendermint_16600-1
-0gchaind config keyring-backend test
-0gchaind config node tcp://localhost:26657
-
-# Initialize the node
-0gchaind init "$OG_ALIAS" --chain-id zgtendermint_16600-1
-
-# Download genesis and addrbook files
-curl -L https://snapshots-testnet.nodejumper.io/0g-testnet/genesis.json > $HOME/.0gchain/config/genesis.json
-curl -L https://snapshots-testnet.nodejumper.io/0g-testnet/addrbook.json > $HOME/.0gchain/config/addrbook.json
-
-# Set seeds
-sed -i -e 's|^seeds *=.*|seeds = "c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656"|' $HOME/.0gchain/config/config.toml
-
-# Set minimum gas price
-sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0025ua0gi"|' $HOME/.0gchain/config/app.toml
-
-# Set pruning
-sed -i \
-  -e 's|^pruning *=.*|pruning = "custom"|' \
-  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
-  $HOME/.0gchain/config/app.toml
-
-# Download latest chain data snapshot
-curl "https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.0gchain"
-#change ports
-sed -i.bak -e "s%:26658%:33658%; s%:26657%:33657%; s%:6060%:6760%; s%:26656%:33656%; s%:26660%:33660%" $HOME/.0gchain/config/config.toml && sed -i.bak -e "s%:9090%:9790%; s%:9091%:9791%; s%:1317%:2017%; s%:8545%:9245%; s%:8546%:9246%; s%:6065%:6765%" $HOME/.0gchain/config/app.toml && sed -i.bak -e "s%:26657%:33657%" $HOME/.0gchain/config/client.toml 
+sed -i 's|miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|' $HOME/0g-storage-node/run/config.toml
+#Config
+sed -i 's|# log_config_file = "log_config"|log_config_file = "'"$ZGS_LOG_CONFIG_FILE"'"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|# log_directory = "log"|log_directory = "'"$ZGS_LOG_DIR"'"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|mine_contract_address = ".*"|mine_contract_address = "'"$MINE_CONTRACT"'"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|log_contract_address = ".*"|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|blockchain_rpc_endpoint = "https://rpc-testnet.0g.ai"|blockchain_rpc_endpoint = "https://0g-evm.rpc.nodebrand.xyz"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|# network_dir = "network"|network_dir = "network"|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|# network_libp2p_port = 1234|network_libp2p_port = 1234|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmPxGNWu9eVAQPJww79J32pTJLKGcpjRMb4Qb8xxKkyuG1","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAm93Hd5azfhkGBbkx1zero3nYHvfjQYM2NtiW4R3r5bE2g"\]|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|' $HOME/0g-storage-node/run/config.toml
+sed -i 's|# db_dir = "db"|db_dir = "db"|' $HOME/0g-storage-node/run/config.toml
 # Create a service
-sudo tee /etc/systemd/system/0gchaind.service > /dev/null << EOF
+sudo tee /etc/systemd/system/zgs.service > /dev/null <<EOF
 [Unit]
-Description=0G node service
-After=network-online.target
+Description=ZGS Node
+After=network.target
+
 [Service]
-User=$USER
-ExecStart=$(which 0gchaind) start
+User=root
+WorkingDirectory=$HOME/0g-storage-node/run
+ExecStart=$HOME/0g-storage-node/target/release/zgs_node --config $HOME/0g-storage-node/run/config.toml
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
-sudo systemctl daemon-reload
-sudo systemctl enable 0gchaind.service
-cd $HOME
-# Start the service and check the logs
-sudo systemctl start 0gchaind.service
-sudo journalctl -u 0gchaind.service -f --no-hostname -o cat
-############
+# Start the service
+sudo systemctl daemon-reload && \
+sudo systemctl enable zgs && \
+sudo systemctl start zgs
+
 
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-    sudo systemctl disable 0gchaind.service
-    sudo rm /etc/systemd/system/0gchaind.service
-    sudo rm -rf $HOME/.0gchain $HOME/0gchain
+    sudo systemctl disable zgs.service
+    sudo rm /etc/systemd/system/zgs.service
+    sudo rm -rf $HOME/0g-storage-node
     echo "Done"
     cd $HOME
     ;;
