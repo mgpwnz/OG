2abf70a46945581f1cd6e6ece1c4edc626b19975
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-06-16 17:56:29 +0300

    upd

--- storage.sh
+++ storage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Default variables
 function="install"
+ENR_ADDRESS=$(wget -qO- eth0.me)
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
@@ -28,7 +29,7 @@ cd $HOME
 sudo apt update
 sudo apt install make unzip clang pkg-config git-core libudev-dev libssl-dev build-essential libclang-12-dev git jq ncdu bsdmainutils htop -y < "/dev/null"
 sleep 1
-VERSION=1.21.3
+VERSION=1.22.0
 wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
@@ -38,41 +39,55 @@ echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile &
 go version
 
 cd $HOME
-# Clone project repository
-git clone -b v0.2.0 https://github.com/0glabs/0g-storage-node.git
-cd $HOME/0g-storage-node
-git submodule update --init
-sudo apt install cargo
-cargo build --release
-# var
+#set vars
+echo "export ENR_ADDRESS=${ENR_ADDRESS}" >> ~/.bash_profile
 echo 'export ZGS_LOG_DIR="$HOME/0g-storage-node/run/log"' >> ~/.bash_profile
-echo 'export ZGS_LOG_CONFIG_FILE="$HOME/0g-storage-node/run/log_config"' >> ~/.bash_profile
-echo 'export LOG_CONTRACT_ADDRESS="0x2b8bC93071A6f8740867A7544Ad6653AdEB7D919"' >> ~/.bash_profile
-echo 'export MINE_CONTRACT="0x228aCfB30B839b269557214216eA4162db24445d"' >> ~/.bash_profile
+echo 'export ZGS_LOG_SYNC_BLOCK="334797"' >> ~/.bash_profile
+echo 'export LOG_CONTRACT_ADDRESS="0xb8F03061969da6Ad38f0a4a9f8a86bE71dA3c8E7"' >> ~/.bash_profile
+echo 'export MINE_CONTRACT="0x96D90AAcb2D5Ab5C69c1c351B0a0F105aae490bE"' >> ~/.bash_profile
+echo 'export BLOCKCHAIN_RPC_ENDPOINT="<your json rpc endpoint>"' >> ~/.bash_profile
 
 source ~/.bash_profile
 
-read -sp "Enter your private key: " PRIVATE_KEY && echo
+echo -e "\n\033[31mCHECK YOUR STORAGE NODE VARIABLES\033[0m\n\nLOG_CONTRACT_ADDRESS: $LOG_CONTRACT_ADDRESS\nMINE_CONTRACT: $MINE_CONTRACT\nZGS_LOG_SYNC_BLOCK: $ZGS_LOG_SYNC_BLOCK\nBLOCKCHAIN_RPC_ENDPOINT: $BLOCKCHAIN_RPC_ENDPOINT\n\n"
 
-sed -i 's|miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|' $HOME/0g-storage-node/run/config.toml
-#Config
-sed -i 's|# log_config_file = "log_config"|log_config_file = "'"$ZGS_LOG_CONFIG_FILE"'"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|# log_directory = "log"|log_directory = "'"$ZGS_LOG_DIR"'"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|mine_contract_address = ".*"|mine_contract_address = "'"$MINE_CONTRACT"'"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|log_contract_address = ".*"|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|blockchain_rpc_endpoint = "https://rpc-testnet.0g.ai"|blockchain_rpc_endpoint = "https://0g-evm.rpc.nodebrand.xyz"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|# network_dir = "network"|network_dir = "network"|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|# network_libp2p_port = 1234|network_libp2p_port = 1234|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmPxGNWu9eVAQPJww79J32pTJLKGcpjRMb4Qb8xxKkyuG1","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAm93Hd5azfhkGBbkx1zero3nYHvfjQYM2NtiW4R3r5bE2g"\]|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|' $HOME/0g-storage-node/run/config.toml
-sed -i 's|# db_dir = "db"|db_dir = "db"|' $HOME/0g-storage-node/run/config.toml
-# Create a service
+#download binary
+cd $HOME
+git clone https://github.com/0glabs/0g-storage-node.git
+cd $HOME/0g-storage-node
+git fetch
+git checkout tags/v0.3.1
+git submodule update --init
+sudo apt install cargo
+cargo build --release
+# PK
+read -sp "Enter your private key: " PRIVATE_KEY && echo
+sed -i '
+s|^miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|
+s|^\s*#\?\s*network_dir\s*=.*|network_dir = "network"|
+s|^\s*#\?\s*network_enr_address\s*=.*|network_enr_address = "'"$ENR_ADDRESS"'"|
+s|^\s*#\?\s*network_enr_tcp_port\s*=.*|network_enr_tcp_port = 1234|
+s|^\s*#\?\s*network_enr_udp_port\s*=.*|network_enr_udp_port = 1234|
+s|^\s*#\?\s*network_libp2p_port\s*=.*|network_libp2p_port = 1234|
+s|^\s*#\?\s*network_discovery_port\s*=.*|network_discovery_port = 1234|
+s|^\s*#\?\s*rpc_enabled\s*=.*|rpc_enabled = true|
+s|^\s*#\?\s*db_dir\s*=.*|db_dir = "db"|
+s|^\s*#\?\s*log_config_file\s*=.*|log_config_file = "log_config"|
+s|^\s*#\?\s*log_directory\s*=.*|log_directory = "log"|
+s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|
+s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|
+s|^\s*#\?\s*mine_contract_address\s*=.*|mine_contract_address = "'"$MINE_CONTRACT"'"|
+s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = '"$ZGS_LOG_SYNC_BLOCK"'|
+s|^\s*#\?\s*blockchain_rpc_endpoint\s*=.*|blockchain_rpc_endpoint = "'"$BLOCKCHAIN_RPC_ENDPOINT"'"|
+' $HOME/0g-storage-node/run/config.toml
+#create service
 sudo tee /etc/systemd/system/zgs.service > /dev/null <<EOF
 [Unit]
 Description=ZGS Node
 After=network.target
 
 [Service]
-User=root
+User=$USER
 WorkingDirectory=$HOME/0g-storage-node/run
 ExecStart=$HOME/0g-storage-node/target/release/zgs_node --config $HOME/0g-storage-node/run/config.toml
 Restart=on-failure
@@ -82,20 +97,21 @@ LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-# Start the service
+#start the node
 sudo systemctl daemon-reload && \
 sudo systemctl enable zgs && \
-sudo systemctl start zgs
-
+sudo systemctl start zgs && \
+sudo systemctl status zgs
 
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-    sudo systemctl disable zgs.service
+    sudo systemctl stop zgs
+    sudo systemctl disable zgs
     sudo rm /etc/systemd/system/zgs.service
-    sudo rm -rf $HOME/0g-storage-node
+    rm -rf $HOME/0g-storage-node
     echo "Done"
     cd $HOME
     ;;
