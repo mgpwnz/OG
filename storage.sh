81ded36ed4bec56f698648073db15d823bb207d4
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-07-01 21:23:43 +0300

    update

--- storage.sh
+++ storage.sh
@@ -56,7 +56,7 @@ cd $HOME
 git clone https://github.com/0glabs/0g-storage-node.git
 cd $HOME/0g-storage-node
 git fetch
-git checkout tags/v0.3.1
+git checkout tags/v0.3.2
 git submodule update --init
 sudo apt install cargo
 cargo build --release
@@ -68,13 +68,16 @@ s|^\s*#\?\s*network_dir\s*=.*|network_dir = "network"|
 s|^\s*#\?\s*network_enr_address\s*=.*|network_enr_address = "'"$ENR_ADDRESS"'"|
 s|^\s*#\?\s*network_enr_tcp_port\s*=.*|network_enr_tcp_port = 1234|
 s|^\s*#\?\s*network_enr_udp_port\s*=.*|network_enr_udp_port = 1234|
+s|^\s*#\s*watch_loop_wait_time_ms\s*=.*|watch_loop_wait_time_ms = 1000|
 s|^\s*#\?\s*network_libp2p_port\s*=.*|network_libp2p_port = 1234|
 s|^\s*#\?\s*network_discovery_port\s*=.*|network_discovery_port = 1234|
+s|^\s*#\s*rpc_listen_address\s*=.*|rpc_listen_address = "0.0.0.0:5678"|
+s|^\s*#\s*rpc_listen_address_admin\s*=.*|rpc_listen_address_admin = "0.0.0.0:5679"|
 s|^\s*#\?\s*rpc_enabled\s*=.*|rpc_enabled = true|
 s|^\s*#\?\s*db_dir\s*=.*|db_dir = "db"|
 s|^\s*#\?\s*log_config_file\s*=.*|log_config_file = "log_config"|
 s|^\s*#\?\s*log_directory\s*=.*|log_directory = "log"|
-s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|
+s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmPxGNWu9eVAQPJww79J32pTJLKGcpjRMb4Qb8xxKkyuG1","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAm93Hd5azfhkGBbkx1zero3nYHvfjQYM2NtiW4R3r5bE2g","/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|
 s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|
 s|^\s*#\?\s*mine_contract_address\s*=.*|mine_contract_address = "'"$MINE_CONTRACT"'"|
 s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = '"$ZGS_LOG_SYNC_BLOCK"'|
